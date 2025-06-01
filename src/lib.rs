ea1625b0b36a2f2723790f3dcd7b36f534dd3e6b
hardcoreholdem <hardcoreholdem@gmail.com>
2025-06-02 01:17:43 +0800

    expose hand rank

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ mod types;
 pub use equitizer::Equitizer;
 pub use hand_ranker::HandRanker;
 pub use types::Card;
+pub use types::HandRank;
 pub use types::MixedRange;
 pub use types::PureRange;
 pub use types::Range;
